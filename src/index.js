362ca4ae9caeaddd57da29f90a0011e942f44906
zysallen <243816351@qq.com>
2018-03-04 18:06:22 +0800

    firet commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react'
+import {isEqual} from 'lodash'
+const watcher = (Comp) => {
+    const didUpdate = Comp.prototype.componentDidUpdate
+    return class extends Comp {
+        componentDidUpdate(props,state) {
+            for(let value in this.state.watch) {
+                let cb = this.state.watch[value]
+                let [newVal,oldVal] = [this.state[value],state[value]]
+                if(!isEqual(newVal,oldVal)) {
+                    cb.call(this,newVal,oldVal)
+                }
+            }
+            if (didUpdate) {
+                didUpdate.call(this, props,state)
+            }
+        }
+        render() {
+            return super.render()
+        }
+    }
+}
+export default watcher
